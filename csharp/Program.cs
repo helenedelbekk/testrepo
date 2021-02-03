e0296c95800cd3f60cc3b892642d584c4a4a0674
Helene Delbekk <delbekk.helene@gmail.com>
2021-02-03 13:09:40 +0100

    Initial check-in of module csharp

---
+++ csharp/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace csharp
+{
+    class MainClass
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string a = "Hello there";
+            string b = "Sigurd";
+            Console.WriteLine(a+ " " +b);
+        }
+    }
+}
